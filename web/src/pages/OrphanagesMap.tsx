4078c1d2829b5158083cf3f9536850bec740b46e
João Lucas Mota <joaomota59@hotmail.com>
2020-10-14 00:43:08 -0300

    Finalização web do primeiro dia - NLW - Concluído

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import {FiPlus} from 'react-icons/fi'
+import {Map, TileLayer} from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+
+import mapMarkerImg from '../images/map-maker.svg'
+
+import '../styles/pages/orphanage-map.css'
 
 function OrphanagesMap(){
     return (
-        <h1>Hello Wolrd</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita</p>
+
+                </header>
+                <footer>
+                    <strong>Rio do Sul</strong>
+                    <span>Santa Catarina</span>
+                </footer>
+            </aside>
+            
+            <Map  
+                center={[-5.1366249,-42.8123643]}
+                zoom={15}
+                style={{width:'100%', height:'100%'}}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+                
+        </div>
     )
 }
 
